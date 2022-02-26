e30709655ea6f3affea6346f81a564f92605d04d
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-02-25 22:07:51 -0300

    Criado funcões no js para gerar senhar e copiar

---
+++ script.js
@@ -0,0 +1,32 @@
+let sliderEle = document.querySelector('#slider')
+let btn = document.querySelector('#btn')
+let sizePassword = document.querySelector('#valor')
+let passWord = document.querySelector('#password')
+let container = document.querySelector('#hide')
+
+let charset = "absdefghijklmnopqrstuvxyzwABCDEFGHIJLMNOPQRSTUVXYZWK@!"
+let novaSenha = ""
+
+sizePassword.innerHTML = sliderEle.value
+
+slider.oninput  = function(){
+    sizePassword.innerHTML = this.value
+} 
+
+function genaratePassword(){
+
+    let pass = ""
+    for( let i = 0, n = charset.length; i < sliderEle.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    container.classList.remove("hide")
+    passWord.innerHTML = pass
+    novaSenha = pass
+
+}
+
+function copyPassword(){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha)
+}
